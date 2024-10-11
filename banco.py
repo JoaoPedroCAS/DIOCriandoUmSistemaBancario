cc60adc3f291c13dd2a773e9167e02af9d63d855
Joao Pedro Cardoso Amancio de Sa <70914320+JoaoPedroCAS@users.noreply.github.com>
2024-10-10 22:18:02 -0300

    updated: banco.py

--- banco.py
+++ banco.py
@@ -1,6 +1,10 @@
+from datetime import datetime
+
 saldo = 0.0
 saques_permitidos = 3
 LIMITE = 500
+transacoes_diarias = 0
+LIMITE_TRANSACOES = 10
 extrato = []
 menu = """
 
@@ -19,12 +23,14 @@ def deposito(valor):
 
 def saque(valor, saldo):
     if valor < 0:
-        print("Impossivel sacar valor negativo!")
+        print("Impossível sacar valor negativo!")
+        return False
     if saques_permitidos == 0:
         print("Você não pode sacar mais nenhum valor hoje!")
         return False
     if valor > LIMITE:
         print(f"O valor R${valor:.2f} excede o limite de R${LIMITE:.2f}!")
+        return False
     if valor > saldo:
         print(f"O valor de R${valor:.2f} excede o saldo em conta de R${saldo:.2f}")
         return False
@@ -33,31 +39,39 @@ def saque(valor, saldo):
 def mostrar_extrato(extrato):
     if not extrato:
         print("Não foram realizadas movimentações.")
-    
     else:  
         for item in extrato:
             print(item)
 
-
 while True:
+    if transacoes_diarias >= LIMITE_TRANSACOES:
+        print("Você excedeu o número de transações permitidas para hoje.")
+        break
+
     escolha = input(menu)
+    
     if escolha == "d":
         valor = float(input("Digite o valor que deseja depositar: "))
         if deposito(valor):
             saldo += valor
-            extrato.append(f"Depósito: R${valor:.2f}")
-            #print(saldo, extrato)
+            transacoes_diarias += 1
+            data_hora = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+            extrato.append(f"{data_hora} - Depósito: R${valor:.2f}")
+    
     elif escolha == "s":
         valor = float(input("Digite o valor que deseja sacar: "))
         if saque(valor, saldo):
             saldo -= valor
-            extrato.append(f"Saque: R${valor:.2f}")
+            transacoes_diarias += 1
+            data_hora = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+            extrato.append(f"{data_hora} - Saque: R${valor:.2f}")
             saques_permitidos -= 1
-            #print(saldo, extrato)
+    
     elif escolha == "e":
         mostrar_extrato(extrato)
+    
     elif escolha == "q":
         break
-    else:
-        print("Operação invalida")
     
+    else:
+        print("Operação inválida")
